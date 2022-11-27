a38b8261cdb399f3c4aac6c4d22fe77ba9386b37
jeonga <jjeong0106@gmail.com>
2022-11-27 15:52:23 +0900

    [feat] Spring Security 도입, SecurityConfig 추가

--- src/main/java/com/ja/wantrip/WantripApplication.java
+++ src/main/java/com/ja/wantrip/WantripApplication.java
@@ -1,9 +1,15 @@
 package com.ja.wantrip;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableBatchProcessing
+@EnableJpaAuditing
 public class WantripApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ja/wantrip/app/member/controller/MemberController.java
+++ src/main/java/com/ja/wantrip/app/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.ja.wantrip.app.member.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/member")
 public class MemberController {
+    @PreAuthorize("isAnonymous()")
     @GetMapping("/login")
     public String showLogin() {
         return "login";
